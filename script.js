9a99660d46236a66c0e1bb21363e86a964ec7803
Joon <choojoonlim@gmail.com>
2024-12-18 21:08:58 +0800

    added createGrid and checkDraw functions. will need to work on creating a way for the player to input and turn management

--- script.js
+++ script.js
@@ -11,31 +11,15 @@ class Gameboard{
         this.grid[row][col] = symbol
     }
 
-    checkWin(){
-        //check rows
-        for(let row=0; row<3; row++){
-            if(this.grid[row][0] == this.grid[row][1] && this.grid[row][1] == this.grid[row][2]) {
-                return true
-            }
-        }
-
-        //check columns
-        for(let col=0; col<3; col++){
-            if(this.grid[0][col] == this.grid[1][col] && this.grid[1][col] == this.grid[2][col]){
-                return true
-            }
-        }
-
-        //check diagonals
-        if(this.grid[0][0] == this.grid[1][1] && this.grid[1][1] == this.grid[2][2]){
-            return true
-        } else if (this.grid[0][2] == this.grid[1][1] && this.grid[1][1] == this.grid[2][0]){
-            return true
-        } else {
-            return false
-        }
+    createGrid(){
+        for (let i = 0; i < 9; i++){
+            const cell = document.createElement("div")
+            cell.className = "cell"
+            const container = document.querySelector(".grid")
+            container.appendChild(cell)
+    
     }
-
+}
 }
 
 class Player{
@@ -43,20 +27,48 @@ class Player{
         this.name = name
         this.symbol = symbol
     }
-
-    makeMove(){
-    }
 }
 
 class Game{
     constructor(){
         this.gameboard = new Gameboard()
-        
+        this.turn = 0
     }
 
-    getPosition(row, col){
-        return this.gameboard.grid[row][col];
-    }
+    checkWin(){
+        //check rows
+        for(let row=0; row<3; row++){
+            if(this.gameboard.grid[row][0] == this.gameboard.grid[row][1] && this.gameboard.grid[row][1] == this.gameboard.grid[row][2]) {
+                console.log("WIN")
+                return true
+            }
+        }
 
+        //check columns
+        for(let col=0; col<3; col++){
+            if(this.gameboard.grid[0][col] == this.gameboard.grid[1][col] && this.gameboard.grid[1][col] == this.gameboard.grid[2][col]){
+                console.log("WIN")
+                return true
+            }
+        }
+
+        //check diagonals
+        if(this.gameboard.grid[0][0] == this.gameboard.grid[1][1] && this.gameboard.grid[1][1] == this.gameboard.grid[2][2]){
+            console.log("WIN")
+            return true
+        } else if (this.gameboard.grid[0][2] == this.gameboard.grid[1][1] && this.gameboard.grid[1][1] == this.gameboard.grid[2][0]){
+            console.log("WIN")
+            return true
+        }
+    }
+    
+    checkDraw(){
+        if(this.turn === 9){
+            console.log("DRAW")
+            return true
+        } else {
+            return false
+        }
+    }
 
 }
