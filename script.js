d2a80c19c75fb37b6d616ae32b42e008d69d25f0
Joon <choojoonlim@gmail.com>
2025-01-06 20:41:49 +0800

    added the makeMove game logic that will handle the marking of symbols on the specified cell using info from the UX

--- script.js
+++ script.js
@@ -10,16 +10,6 @@ class Gameboard{
     updateBoard(row, col, symbol){
         this.grid[row][col] = symbol
     }
-
-    createGrid(){
-        for (let i = 0; i < 9; i++){
-            const cell = document.createElement("div")
-            cell.className = "cell"
-            const container = document.querySelector(".grid")
-            container.appendChild(cell)
-    
-    }
-}
 }
 
 class Player{
@@ -33,12 +23,14 @@ class Game{
     constructor(){
         this.gameboard = new Gameboard()
         this.turn = 0
+        this.player1 = new Player("Myself", "X")
+        this.player2 = new Player("Opponent", "O")
     }
 
     checkWin(){
         //check rows
         for(let row=0; row<3; row++){
-            if(this.gameboard.grid[row][0] == this.gameboard.grid[row][1] && this.gameboard.grid[row][1] == this.gameboard.grid[row][2]) {
+            if(this.gameboard.grid[row][0] === this.gameboard.grid[row][1] && this.gameboard.grid[row][1] === this.gameboard.grid[row][2]) {
                 console.log("WIN")
                 return true
             }
@@ -46,17 +38,17 @@ class Game{
 
         //check columns
         for(let col=0; col<3; col++){
-            if(this.gameboard.grid[0][col] == this.gameboard.grid[1][col] && this.gameboard.grid[1][col] == this.gameboard.grid[2][col]){
+            if(this.gameboard.grid[0][col] === this.gameboard.grid[1][col] && this.gameboard.grid[1][col] === this.gameboard.grid[2][col]){
                 console.log("WIN")
                 return true
             }
         }
 
         //check diagonals
-        if(this.gameboard.grid[0][0] == this.gameboard.grid[1][1] && this.gameboard.grid[1][1] == this.gameboard.grid[2][2]){
+        if(this.gameboard.grid[0][0] === this.gameboard.grid[1][1] && this.gameboard.grid[1][1] === this.gameboard.grid[2][2]){
             console.log("WIN")
             return true
-        } else if (this.gameboard.grid[0][2] == this.gameboard.grid[1][1] && this.gameboard.grid[1][1] == this.gameboard.grid[2][0]){
+        } else if (this.gameboard.grid[0][2] === this.gameboard.grid[1][1] && this.gameboard.grid[1][1] === this.gameboard.grid[2][0]){
             console.log("WIN")
             return true
         }
@@ -71,4 +63,15 @@ class Game{
         }
     }
 
+    makeMove(){
+        if (this.turn % 2 === 0){
+            updateBoard(row, col, "X")
+        }
+        else{
+            console.log("no")
+            updateBoard(row, col, "O")
+        }
+        this.turn +=1
+    }
 }
+const game= new Game()
